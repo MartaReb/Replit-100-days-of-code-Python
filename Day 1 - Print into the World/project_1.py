4a0917758b4e5668a4353f77ac27dccc7a7188ef
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-02-07 13:44:39 +0100

    Initial commit

---
+++ Day 1 - Print into the World/project_1.py
@@ -0,0 +1,5 @@
+print("Marta")
+print("07.02.2024")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.""")
+print("I am feeling 🥳")
