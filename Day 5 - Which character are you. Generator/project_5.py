150faa9bfd852ddd3cefa57f5478c0d0c8b8233f
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-02-09 13:48:44 +0100

    add project_5

---
+++ Day 5 - Which character are you. Generator/project_5.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print("--"*15)
+hanging_around = input("Do you like 'hanging around'?: ")
+if hanging_around == "No":
+  print("Then you're not Spider-man.")
+else:
+  print("You are Spider-man!")
+gravelly = input("Do you have a 'gravelly' voice?: ")
+if gravelly == "No":
+  print("Aww, then you're not Korg.")
+else:
+  print("You are Korg!")
+marvelous = input("Do you often feel 'Marvelous'?: ")
+if marvelous =="Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You're Captain Marvel! :(")
