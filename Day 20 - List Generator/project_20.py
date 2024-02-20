9c8ec76ad58b16c92e744201fd99762575c22be5
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-02-20 11:52:34 +0100

    add project_20

---
+++ Day 20 - List Generator/project_20.py
@@ -0,0 +1,10 @@
+print("List Generator")
+print()
+print("You are going to give me a number you want to start with, an ending number, and by how many you want me to add each time.")
+print()
+start = int(input("Start at: "))
+end = int(input("End before: "))
+increment = int (input("Increment between values: "))
+print()
+for i in range(start, end, increment):
+  print(i)
