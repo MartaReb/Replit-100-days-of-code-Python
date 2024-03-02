844d417d9a0c8e6a8607325ef229d1a466d8edb7
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-03-02 11:39:55 +0100

    add project_32

---
+++ Day 32 - Greetings/project_32.py
@@ -0,0 +1,6 @@
+import random
+
+greetings = ["Cześć", "Hello", "Salut", "Ciao", "Anyoung", "Yassou", "Habari", "Ahlan", "Hallo", "Tjena", "Hej"]
+
+number = random.randint(0,10)
+print (greetings[number])
