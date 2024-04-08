a876fe978da37a39b1c09f21ae90e1f8e335eaa1
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-04-08 20:33:45 +0200

    change how to retrieve the leader

--- Day 49 -Current Leader/project_49.py
+++ Day 49 -Current Leader/project_49.py
@@ -1,18 +1,19 @@
 print("🌟Current Leader🌟")
 print()
 print("Analyzing high scores......")
+print()
 f = open("high.score", "r")
-list_of_scores = []
-list_of_users = []
-while True:
-  contents = f.readline().strip()
-
-  if contents == "":
-    break
-  user, score = contents.split() 
-  list_of_scores.append(int(score))
-  list_of_users.append(user)
-  
-  print(f"Current leader is {user}." )
-
+scores = f.read().split("\n")
 f.close()
+
+max_score = 0
+name = ""
+
+for row in scores:
+  data = row.split()
+  if data != []:
+    if int(data[1]) > max_score:
+      max_score = int(data[1])
+      name = data[0]
+              
+print("Current leader is", name, "with", max_score, "scores.")
