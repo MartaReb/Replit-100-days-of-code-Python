bf18ea9b095e00ac16d6db4a66af50cd51bc0a68
MartaReb <49365894+MartaReb@users.noreply.github.com>
2024-05-11 19:24:30 +0200

    add project_60

---
+++ Day 60 - Event Countdown Timer/project_60.py
@@ -0,0 +1,20 @@
+import datetime
+
+print("Event Countdown Timer")
+print()
+today = datetime.date.today()
+event_name = input("What is the name of the event? ")
+event_year = int(input("What year is the event? "))
+event_month = int(input("What month is the event? "))
+event_day = int(input("What day is the event? "))
+event_date = datetime.date(event_year, event_month, event_day)
+
+difference = event_date - today
+difference = difference.days
+
+if today == event_date:
+  print(f"🎉🎉 {event_name} is today! 🎉🎉")
+elif today > event_date:
+  print(f"😢😢 {event_name} was {difference} days ago. 😢😢")
+else:
+  print(f"😊😊 {event_name} is in {difference} days. 😊😊")
